a0dcab53c684dab962605d2d26df167586a657b5
Elisheba <elisheba.wiggins@priviahealth.com>
2017-11-05 17:30:31 -0500

    Add styling to College Financial Chart

--- app/controllers/mpayments_controller.rb
+++ app/controllers/mpayments_controller.rb
@@ -1,10 +1,9 @@
 class MpaymentsController < ApplicationController
   require 'json'
   def index
-   @payments = HTTParty.get('https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,2013.aid.median_debt.completers.overall,2013.repayment.1_yr_repayment.completers,2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings&api_key=IOS6e2D8nxLIcWJWn5tFIgiRsDwkmREhYemwId50',
-   :headers =>{'Content-Type' => 'application/json'} )
+    @payments = HTTParty.get('https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,2013.aid.median_debt.completers.overall,2013.aid.median_debt_suppressed.completers.monthly_payments,2013.repayment.1_yr_repayment.completers,2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings&api_key=IOS6e2D8nxLIcWJWn5tFIgiRsDwkmREhYemwId50',
+    :headers =>{'Content-Type' => 'application/json'} )
 
-   @parsed_payments = @payments['results']
-   @parsed_name = @parsed_payments[1]
- end
+    @parsed_payments = @payments['results']
+  end
 end
