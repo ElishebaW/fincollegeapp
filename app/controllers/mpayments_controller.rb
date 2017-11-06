cd57f89d017fb65ccc1f71aeb96b3cb922494cd4
Elisheba <elisheba.wiggins@priviahealth.com>
2017-11-06 12:59:57 -0500

    WIP

--- app/controllers/mpayments_controller.rb
+++ app/controllers/mpayments_controller.rb
@@ -1,7 +1,7 @@
 class MpaymentsController < ApplicationController
   require 'json'
   def index
-    @payments = HTTParty.get('https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,2013.aid.median_debt.completers.overall,2013.aid.median_debt_suppressed.completers.monthly_payments,2013.repayment.1_yr_repayment.completers,2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings&api_key=IOS6e2D8nxLIcWJWn5tFIgiRsDwkmREhYemwId50',
+    @payments = HTTParty.get('https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,2013.aid.median_debt.completers.overall,2013.aid.median_debt_suppressed.completers.monthly_payments,2013.repayment.1_yr_repayment.completers,2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings&page=95&api_key=IOS6e2D8nxLIcWJWn5tFIgiRsDwkmREhYemwId50',
     :headers =>{'Content-Type' => 'application/json'} )
 
     @parsed_payments = @payments['results']
